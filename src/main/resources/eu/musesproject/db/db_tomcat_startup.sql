885181680afe87766e0c050fd2c56ec2b805ccae
Vahid Shirvani <vahid_shirvani@hotmail.com>
2015-03-26 12:56:19 +0100

    added the database script for tomcat realm

---
+++ src/main/resources/eu/musesproject/db/db_tomcat_startup.sql
@@ -0,0 +1,49 @@
+DROP DATABASE IF EXISTS tomcat_realm;
+CREATE DATABASE tomcat_realm;
+USE tomcat_realm;
+CREATE TABLE users (
+username varchar(20) NOT NULL PRIMARY KEY,
+password varchar(20) NOT NULL
+);
+CREATE TABLE roles (
+rolename varchar(20) NOT NULL PRIMARY KEY
+);
+CREATE TABLE users_roles (
+username varchar(20) NOT NULL,
+rolename varchar(20) NOT NULL,
+PRIMARY KEY (username, rolename),
+CONSTRAINT users_roles_fk1 FOREIGN KEY (username) REFERENCES users (username),
+CONSTRAINT users_roles_fk2 FOREIGN KEY (rolename) REFERENCES roles (rolename)
+);
+INSERT INTO `tomcat_realm`.`users` (`username`, `password`) VALUES ('musesadmin', 'musespass');
+INSERT INTO `tomcat_realm`.`roles` (`rolename`) VALUES ('manager-gui');
+INSERT INTO `tomcat_realm`.`roles` (`rolename`) VALUES ('manager-script');
+INSERT INTO `tomcat_realm`.`roles` (`rolename`) VALUES ('manager');
+INSERT INTO `tomcat_realm`.`roles` (`rolename`) VALUES ('admin-gui');
+INSERT INTO `tomcat_realm`.`roles` (`rolename`) VALUES ('admin-script');
+INSERT INTO `tomcat_realm`.`roles` (`rolename`) VALUES ('admin');
+INSERT INTO `tomcat_realm`.`users_roles` (`username`, `rolename`) VALUES ('musesadmin', 'manager-gui');
+INSERT INTO `tomcat_realm`.`users_roles` (`username`, `rolename`) VALUES ('musesadmin', 'manager-script');
+INSERT INTO `tomcat_realm`.`users_roles` (`username`, `rolename`) VALUES ('musesadmin', 'manager');
+INSERT INTO `tomcat_realm`.`users_roles` (`username`, `rolename`) VALUES ('musesadmin', 'admin-gui');
+INSERT INTO `tomcat_realm`.`users_roles` (`username`, `rolename`) VALUES ('musesadmin', 'admin-script');
+INSERT INTO `tomcat_realm`.`users_roles` (`username`, `rolename`) VALUES ('musesadmin', 'admin');
+COMMIT;
+
+CREATE USER 'tomcat'@'localhost' IDENTIFIED BY 'tomcat';
+GRANT ALL PRIVILEGES ON tomcat_realm.* TO 'tomcat'@'localhost' WITH GRANT OPTION;
+
+/* Place mysql-connector-java.jar in tomcat_home/lib */
+/* Place this inside tomcat server.xml */
+/* 
+<Realm  className="org.apache.catalina.realm.JDBCRealm"
+ driverName="com.mysql.jdbc.Driver"
+ connectionURL="jdbc:mysql://localhost:3306/tomcat_realm"
+ connectionName="tomcat"
+ connectionPassword="tomcat"
+ userTable="users"
+ userNameCol="username"
+ userCredCol="password"
+ userRoleTable="users_roles"
+ roleNameCol="rolename" />
+*/
